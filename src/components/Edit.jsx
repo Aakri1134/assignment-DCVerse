a7726df9757a0c228a45d7839e3c5d6a1e19d850
Aakri1134 <aakrisri55@gmail.com>
2025-05-15 22:12:29 +0530

    Add button functionality

--- src/components/Edit.jsx
+++ src/components/Edit.jsx
@@ -1,7 +1,12 @@
 const Edit = () => {
+
+    const handleSubmit = () => {
+
+    }
   return (
     <div className="pt-4 lg:pt-8">
       <button
+      onClick={handleSubmit}
         className="bg-buttonColor w-32 h-10 font-sans font-semibold text-white border-2 border-borderColor rounded-lg border-dashed text-2xl hover:bg-backgroundColor hover:text-black hover:scale-105 active:scale-95 transform transition duration-75"
       >
         Edit
